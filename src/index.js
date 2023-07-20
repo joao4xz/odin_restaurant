2aa86ca6a6894ead9761fd9e403208f6935abf02
joao4xz <joao.4xz@gmail.com>
2023-07-20 09:15:58 -0300

    Add html-webpack-plugin and restaurant html structure

--- src/index.js
+++ src/index.js
@@ -1,11 +1,24 @@
 import './style.css';
+import iceCreamimage from './assets/images/icecream.jpg'
 
 function component() {
-  const element = document.createElement('div');
+  const container = document.createElement('div');
+  container.id = 'content';
 
-  element.innerHTML = 'Hello World!';
+  const h1 = document.createElement('h1');
+  h1.textContent = 'Frosty Delights'
 
-  return element;
+  const p = document.createElement('p');
+  p.textContent = 'Indulge in handcrafted, premium ice creams with a tempting variety of flavors. Friendly staff and a charming atmosphere make every visit a delightful experience in our ice cream wonderland.'
+
+  const image = new Image();
+  image.src = iceCreamimage;
+
+  container.appendChild(h1);
+  container.appendChild(p);
+  container.appendChild(image);
+
+  return container;
 }
 
 document.body.appendChild(component());
