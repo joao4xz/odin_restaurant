c2b9c20e9f2c4085afeb724822ace3a6bbb3d303
joao4xz <joao.4xz@gmail.com>
2023-07-20 11:41:24 -0300

    Add restaurant about section

--- src/index.js
+++ src/index.js
@@ -5,18 +5,44 @@ function component() {
   const container = document.createElement('div');
   container.id = 'content';
 
-  const h1 = document.createElement('h1');
-  h1.textContent = 'Frosty Delights'
+  // Header
+  const header = document.createElement('header');
+
+  const h1Shadow = document.createElement('h1');
+  h1Shadow.textContent = 'Frosty Delights';
+  h1Shadow.id = 'shadow';
+
+  const h1Text = document.createElement('h1');
+  h1Text.textContent = 'Frosty Delights';
+  h1Text.id = 'text';
+
+  const h1Line = document.createElement('h1');
+  h1Line.textContent = 'Frosty Delights';
+  h1Line.id = 'line';
+
+  header.appendChild(h1Shadow);
+  header.appendChild(h1Text);
+  header.appendChild(h1Line);
+
+  // Main
+  const main = document.createElement('main');
+
+  const aboutCard = document.createElement('div');
+  aboutCard.classList.add('aboutCard');
 
   const p = document.createElement('p');
-  p.textContent = 'Indulge in handcrafted, premium ice creams with a tempting variety of flavors. Friendly staff and a charming atmosphere make every visit a delightful experience in our ice cream wonderland.'
+  p.textContent = 'Step into a delightful world of heavenly ice creams, carefully handcrafted with a wonderful variety of flavors. Our friendly team is ready to treat you to a magical experience at our warm and inviting ice cream wonderland. Explore the joy of frosty creations, each made with love to bring smiles to every scoop. Come and enjoy the enchantment, creating unforgettable memories with us at our creamy haven!'
 
   const image = new Image();
   image.src = iceCreamimage;
 
-  container.appendChild(h1);
-  container.appendChild(p);
-  container.appendChild(image);
+  aboutCard.appendChild(p);
+  aboutCard.appendChild(image);
+
+  main.appendChild(aboutCard);
+
+  container.appendChild(header);
+  container.appendChild(main);
 
   return container;
 }
